dc91bcc9e2e06d380ddbafef4cb7ee8d9ae95df3
Fcontreras25 <fcontrerasmtz0@gmail.com>
2024-11-29 17:57:41 -0600

    Se añadio carpeta

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import React from "react";
 import Layout from "./layout";
-import Crearcuenta from "./Crearcuenta";
-import Login from "./Login";
-import Nuevacontra from "./Nuevacontra";
-import Restablecercontra from "./Restablecercontra";
-import Bienvenida from "./Bienvenida";
-import Criptografia1 from "./Criptografia1";
-import Criptografia2 from "./Criptografia2";
-import Criptografia3 from "./Criptografia3";
+import Crearcuenta from "./pages/Crearcuenta";
+import Login from "./pages/Login";
+import Nuevacontra from "./pages/Nuevacontra";
+import Restablecercontra from "./pages/Restablecercontra";
+import Bienvenida from "./pages/Bienvenida";
+import Criptografia1 from "./pages/Criptografia1";
+import Criptografia2 from "./pages/Criptografia2";
+import Criptografia3 from "./pages/Criptografia3";
 
 const App: React.FC = () => {
   return (
