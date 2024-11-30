36b76e96abd99d83a49b9b8fe25b2bb4d1925e45
Fcontreras25 <fcontrerasmtz0@gmail.com>
2024-11-29 18:47:12 -0600

    Cambio de configuracion

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,28 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: 'dist', // Carpeta de salida para los archivos de construcción
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          // Divide los módulos de node_modules para optimizar la carga
+          vendor: ['react', 'react-router-dom'],
+        },
+      },
+    },
+  },
+  server: {
+    open: true, // Abre automáticamente en el navegador al iniciar el servidor
+    port: 3000, // Configura el puerto del servidor de desarrollo
+  },
+  resolve: {
+    alias: {
+      // Alias opcionales para rutas más cortas
+      '@': '/src',
+    },
+  },
+});
